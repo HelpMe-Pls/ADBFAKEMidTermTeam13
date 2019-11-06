231e8aaec7ab8f787d3aafabc8792e09d2a66a7b
Kaiservn <djhoang2013@gmail.com>
2019-11-06 09:43:21 +0700

    Thêm câu 1.3

--- Phần 1/ADBTeam13_DB4O/ADBTeam13_DB4O/Program.cs
+++ Phần 1/ADBTeam13_DB4O/ADBTeam13_DB4O/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Db4objects.Db4o;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,13 @@ namespace ADBTeam13_DB4O
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            // Câu 1.3
+            string DbFileName = "4301104056_EmployeeManager";
+            IObjectContainer db = Db4oEmbedded.OpenFile(DbFileName);
+            db.Close();
         }
     }
 }
