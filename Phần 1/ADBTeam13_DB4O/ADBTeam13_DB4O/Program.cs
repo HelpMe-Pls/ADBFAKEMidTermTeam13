bf68c1a9562653816b65eb9d21408e4a1053a541
Kaiservn <djhoang2013@gmail.com>
2019-11-06 10:14:56 +0700

    Thêm câu 1.6

--- Phần 1/ADBTeam13_DB4O/ADBTeam13_DB4O/Program.cs
+++ Phần 1/ADBTeam13_DB4O/ADBTeam13_DB4O/Program.cs
@@ -16,6 +16,23 @@ namespace ADBTeam13_DB4O
             string DbFileName = "4301104056_EmployeeManager";
             IObjectContainer db = Db4oEmbedded.OpenFile(DbFileName);
             db.Close();
+
+            //Câu 1.6
+            List<Employee> employees = new List<Employee>();
+            db = Db4oEmbedded.OpenFile(DbFileName);
+            Employee template = new Employee(0, null, null, null, null, 0);
+            IObjectSet resultList = db.QueryByExample(template);
+            foreach (Employee obj in resultList)
+            {
+                employees.Add(obj);
+            }
+            db.Close();
+            // xuất kết quả truy vấn
+            Console.WriteLine("Kết quả truy vấn là: ");
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine(emp.ToString());               
+            }
         }
     }
 }
